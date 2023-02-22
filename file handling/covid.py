488a91182d5d3ce0a72624108a4f44a020bd858d
Bartosz Jaworski <83785015+BarJaw@users.noreply.github.com>
2023-02-22 08:36:40 +0000

    Covid task finished

--- file handling/covid.py
+++ file handling/covid.py
@@ -1,22 +1,16 @@
 import csv
-# import awoc
+import pycountry_convert as pc
 
-# my_world = awoc.AWOC()
-
-# italy_data = my_world.get_country_data('DPRK')
-# print(italy_data)
-# import pycountry_convert as pc
-
-# def country_to_continent(country_name):
+def country_to_continent(country_name):
     
-#     country_alpha2 = pc.country_name_to_country_alpha2(country_name)
-#     country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
-#     country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
-#     return country_continent_name
+    country_alpha2 = pc.country_name_to_country_alpha2(country_name)
+    country_continent_code = pc.country_alpha2_to_continent_code(country_alpha2)
+    country_continent_name = pc.convert_continent_code_to_continent_name(country_continent_code)
+    return country_continent_name
 
-# # Example
-# country_name = 'North Korea'
-# print(country_to_continent(country_name))
+# Example
+country_name = 'Australia'
+print(country_to_continent(country_name))
 
 
 with open('file handling/covid_worldwide.csv', 'r', encoding='UTF-8') as file:
@@ -24,10 +18,20 @@ with open('file handling/covid_worldwide.csv', 'r', encoding='UTF-8') as file:
 
     highest_deaths = 0
     highest_deaths_country = ''
-    
+
     number_of_tests = 0
 
     confirmed_cases = 0
+
+    continents = {
+        'Asia' : 0,
+        'Africa' : 0,
+        'Europe' : 0,
+        'North America' : 0,
+        'South America' : 0,
+        'Oceania' : 0
+
+    }
 
     cfr = {}
 
@@ -47,17 +51,31 @@ with open('file handling/covid_worldwide.csv', 'r', encoding='UTF-8') as file:
             confirmed_cases += total_cases
 
             cfr[row['Country']] = round(deaths / total_cases  * 100, 2)
-        print(row['Country'])
+        
+        try:
+            continent = country_to_continent(row['Country'].lstrip())
+            continents[continent] += int(row['Total Test'].replace(',', ''))
+        except:
+            continue
 
+    print(continents)
 
-
-with open('file handling/output.txt', 'w') as file:
+with open('file handling/covid_ans.txt', 'w') as file:
+    
     # answer 1
     file.write(f'1.\nHighest number of deaths: {highest_deaths_country}\n')
+    
     # answer 2
     file.write(f'2.\nCumulative number of tests: {number_of_tests}\n')
+    
+    # answer 3
+    file.write(f'3.\n')
+    for continent in continents:
+        file.write(f'{continent} : {continents[continent]}\n')
+
     # answer 4
     file.write(f'4.\nCumulative number of confirmed cases: {confirmed_cases}\n')
+    
     # answer 5
     file.write(f'5.\n')
     for row in cfr:
@@ -68,9 +86,3 @@ with open('file handling/output.txt', 'w') as file:
     file.write(f'6.\n')
     for row in ans_6:
         file.write(f'{row[0]} : {row[1]}%\n')
-
-    # print(highest_deaths)
-    # print(highest_deaths_country)
-    # print(number_of_tests)
-    # print(confirmed_cases)
-    # print(sorted(cfr.items(), key=lambda x:x[1]))
